b9925c941ea6b5c2112ef6167e9ed6672f7df6d8
shiyangwork <yang.danshi@ly.com>
2026-01-29 11:16:40 +0800

    深度sop开发

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+    outDir: "dist",
     modules: ["@wxt-dev/module-vue"],
     runner: {
         startUrls: ["https://x.com/home"]
